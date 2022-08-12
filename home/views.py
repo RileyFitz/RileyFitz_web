df78c8338d9131408db646de2d3a4e2b2d066293
riley fitz <rileyfitzgibbons@gmail.com>
2022-08-12 09:54:46 -0600

    Add tags to sidebar on homepage.

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.db.models import Count
 from django.views.generic import (
     TemplateView,
     ListView,
@@ -9,6 +10,7 @@ from django.views.generic import (
     DeleteView
 )
 from .models import Post
+from taggit.models import Tag
 
 class index(ListView):
     model = Post
@@ -16,6 +18,17 @@ class index(ListView):
     context_object_name = 'posts'
     ordering = ['-publish_date']
     paginate_by = 10
+
+
+    def get_context_data(self, **kwargs):
+        data = super(index, self).get_context_data(**kwargs)
+
+        # Query tags, and count number of references each has.
+        query = Tag.objects.all()
+        query2 = query.annotate(num_items=Count('taggit_taggeditem_items')).order_by('-num_items').values()
+        data['tags'] = query2
+
+        return data
 
 class PostDetailView(DetailView):
     model = Post
